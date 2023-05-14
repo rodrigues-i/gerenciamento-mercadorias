65c5999874daf71f150baa53b3bd76794e81039d
rodrigues-i <lucas-f250@live.com>
2023-05-14 20:03:11 -0300

    Cria tabela para cadastrar mercadorias no sistema

--- gerenciamento-mercadoria/Controllers/MercadoriaController.cs
+++ gerenciamento-mercadoria/Controllers/MercadoriaController.cs
@@ -1,6 +1,7 @@
 ﻿using gerenciamento_mercadoria.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -21,12 +22,52 @@ namespace gerenciamento_mercadoria.Controllers
                 Nome = mercadoria.Nome,
                 NumeroRegistro = mercadoria.NumeroRegistro,
                 NomeTipo = mercadoria.Tipos != null ? mercadoria.Tipos.TipoNome : null,
-                Tipo = mercadoria.Tipos.TipoId,
+                TipoId = mercadoria.Tipos.TipoId,
                 Descricao = mercadoria.Descricao,
                 Fabricante = mercadoria.Fabricante
             }).ToList();
 
-            return View(mercadoriaVMList);
+            ViewBag.Mercadorias = mercadoriaVMList;
+
+            List<Tipo> tipos = db.Tipoes.ToList();
+            ViewBag.TipoList = new SelectList(tipos, "TipoId", "TipoNome");
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(MercadoriaViewModel model)
+        {
+            try
+            {
+                gerenciaEntities db = new gerenciaEntities();
+                List<Tipo> tipos = db.Tipoes.ToList();
+                ViewBag.TipoList = new SelectList(tipos, "TipoId", "TipoNome");
+
+                Mercadoria mercadoria = new Mercadoria();
+                mercadoria.Nome = model.Nome;
+                mercadoria.NumeroRegistro = model.NumeroRegistro;
+                mercadoria.Fabricante = model.Fabricante;
+                mercadoria.TipoId = model.TipoId;
+                mercadoria.Descricao = model.Descricao;
+
+                db.Mercadorias.Add(mercadoria);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch (DbEntityValidationException ex)
+            {
+                foreach (var entityValidationErrors in ex.EntityValidationErrors)
+                {
+                    foreach (var validationError in entityValidationErrors.ValidationErrors)
+                    {
+                        Console.WriteLine("Property: " + validationError.PropertyName + " Error: " + validationError.ErrorMessage);
+                    }
+                }
+
+                return RedirectToAction("Index");
+            }
         }
     }
 }
