17f68a062779ef4dbd1267f7a1dc89efe7a3de94
rodrigues-i <lucas-f250@live.com>
2023-05-15 13:17:08 -0300

    Cria view para entradas e implementa cadastro de entrada no sistema

---
+++ gerenciamento-mercadoria/Controllers/EntradaController.cs
@@ -0,0 +1,54 @@
+﻿using gerenciamento_mercadoria.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gerenciamento_mercadoria.Controllers
+{
+    public class EntradaController : Controller
+    {
+        // GET: Entrada
+        public ActionResult Index()
+        {
+            gerenciaEntities db = new gerenciaEntities();
+            List<Entrada> entradas = db.Entradas.ToList();
+            EntradaViewModel entradaVM = new EntradaViewModel();
+            List<EntradaViewModel> entradaVMList = entradas.Select(entrada => new EntradaViewModel
+            {
+                EntradaId = entrada.EntradaId,
+                Nome = entrada.Mercadorias.Nome,
+                Quantidade = entrada.Quantidade,
+                Data = entrada.Data,
+                Local = entrada.Local,
+                MercadoriaId = entrada.MercadoriaId
+            }).ToList();
+
+            ViewBag.Entradas = entradaVMList;
+
+            List<Mercadoria> mercadorias = db.Mercadorias.ToList();
+            ViewBag.MercadoriaList = new SelectList(mercadorias, "MercadoriaId", "Nome");
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(EntradaViewModel model)
+        {
+            gerenciaEntities db = new gerenciaEntities();
+            List<Mercadoria> mercadorias = db.Mercadorias.ToList();
+            ViewBag.MercadoriaList = new SelectList(mercadorias, "MercadoriaId", "Nome");
+
+            Entrada entrada = new Entrada();
+            entrada.Quantidade = model.Quantidade;
+            entrada.Data = model.Data;
+            entrada.Local = model.Local;
+            entrada.MercadoriaId = model.MercadoriaId;
+            db.Entradas.Add(entrada);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ gerenciamento-mercadoria/Models/EntradaViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gerenciamento_mercadoria.Models
+{
+    public class EntradaViewModel
+    {
+        public int EntradaId { get; set; }
+        public int Quantidade { get; set; }
+        public DateTime Data { get; set; }
+        public string Local { get; set; }
+        public int MercadoriaId { get; set; }
+
+        // Custom attribute
+        public string Nome { get; set; }
+    }
+}
