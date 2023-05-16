81c7096a362fc7fa06796cd0aa159ff46414b996
rodrigues-i <lucas-f250@live.com>
2023-05-16 20:29:06 -0300

    Cria página com dropdownlist para gerar relatório

---
+++ gerenciamento-mercadoria/Controllers/RelatorioController.cs
@@ -0,0 +1,55 @@
+﻿using gerenciamento_mercadoria.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Objects.SqlClient;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gerenciamento_mercadoria.Controllers
+{
+    public class RelatorioController : Controller
+    {
+        // GET: Relatorio
+        public ActionResult Index()
+        {
+            gerenciaEntities db = new gerenciaEntities();
+            var mesesEntrada = db.Entradas
+                .Select(entrada => SqlFunctions.DatePart("month", entrada.Data))
+                .Distinct()
+                .ToList();
+
+            var mesesSaida = db.Saidas
+                .Select(saida => SqlFunctions.DatePart("month", saida.Data))
+                .Distinct()
+                .ToList();
+
+            var textInfo = CultureInfo.CurrentCulture.TextInfo;
+
+            var meses = new List<SelectListItem>();
+            foreach (var mes in mesesEntrada.Concat(mesesSaida).Distinct())
+            {
+                meses.Add(new SelectListItem
+                {
+                    Text = textInfo.ToTitleCase(CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName((int)mes).ToLower()),
+                    Value = mes.ToString()
+                });
+            }
+
+            ViewBag.Meses = meses;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GerarRelatorio(RelatorioViewModel model)
+        {
+            int selectedMonth = int.Parse(model.Value);
+
+            // perform logic to generate the report based on the selected month
+
+            return View(); // return the view to display the generated report
+        }
+    }
+}
---
+++ gerenciamento-mercadoria/Models/RelatorioViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gerenciamento_mercadoria.Models
+{
+    public class RelatorioViewModel
+    {
+        public string Text { get; set; }
+        public string Value { get; set; }
+    }
+}
